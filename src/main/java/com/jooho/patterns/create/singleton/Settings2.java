836723b675cf83ea8bc863425749802e6f6153ca
DavidEugen <platonicojju@gmail.com>
2022-04-27 21:07:55 +0900

    feature: Singleton Eager initialization
    특징 :
    처음 static field 가 로드 되는 시점에 인스턴스 생성
    
    문제점 :
    인스턴스를 만드는 과정이 길고 메모리를 많이 사용한다면 , 애플리케이션 로딩할때 많은 리소스를 썼음에도 불구하고 실제 사용하는것은 한참 이후인경우
    인스턴스 생성이 메모리를 많이 사용하거나 오래 걸릴 경우.

--- src/main/java/com/jooho/patterns/create/singleton/Settings2.java
+++ src/main/java/com/jooho/patterns/create/singleton/Settings2.java
@@ -1,16 +1,21 @@
 package com.jooho.patterns.create.singleton;
 
 public class Settings2 {
-	private static Settings2 instance;
+	//	private static Settings2 instance;
+	private static final Settings2 INSTANCE = new Settings2();
 
 	private Settings2() {
 	}
 
-	public static synchronized Settings2 getInstance() {
-		if (instance == null) {
-			instance = new Settings2();
-		}
+//	public static synchronized Settings2 getInstance() {
+//		if (instance == null) {
+//			instance = new Settings2();
+//		}
+//
+//		return instance;
+//	}
 
-		return instance;
+	public static Settings2 getInstance() {
+		return INSTANCE;
 	}
 }
