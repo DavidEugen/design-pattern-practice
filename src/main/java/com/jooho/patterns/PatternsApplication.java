2236ecded1dcf4d4af720c58395c5fc3a92e52df
DavidEugen <platonicojju@gmail.com>
2022-04-27 19:17:00 +0900

    Init Files
    기본 파일 세팅

---
+++ src/main/java/com/jooho/patterns/PatternsApplication.java
@@ -0,0 +1,13 @@
+package com.jooho.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsApplication.class, args);
+	}
+
+}
